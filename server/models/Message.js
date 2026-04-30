81e0f189065b87de54acd0311ac81e5df291c163
Malarkodi T T <thenmozhimalarkodi@gmail.com>
2026-04-30 16:16:10 +0530

    Add files via upload

---
+++ server/models/Message.js
@@ -0,0 +1,9 @@
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("✅ MongoDB Connected!"))
+.catch((err) => {
+    console.error("❌ MongoDB Connection Error:");
+    console.error(err.message);
+});
